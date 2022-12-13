5ec4794fe82dc60f664e8892f2c4634d162f0755
Saptarshi Chakraborty <saptarshi.office@gmail.com>
2022-12-13 14:31:23 +0530

    Add animations and finalize color
    * Remove animate.css
    * Add AOS css and js
    * Add animations to elements on window load
    * Change primary color to #635be7

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,16 @@
 	saptarshi.office@gmail.com
 	saptarshi.c@digitallyinked.in
 */
+
+window.addEventListener("load", (event) => {
+	console.log("Welcome to DigitallyInked.");
+	console.log("This site was Designed and Developed by Saptarshi Chakraborty");
+	console.log("Phone: +91 83349 74261");
+	console.log("Email: saptarshi.office@gmail.com");
+	console.log("Email: saptarshi.c@digitallyinked.in");
+
+	AOS.init({
+		delay: 100,
+		duration: 1000
+	});
+})
